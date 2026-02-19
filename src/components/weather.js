c5b9db6353159db7b6e344a1b0a0ec221386b65e
Jorge Grey <jgrey@poligran.edu.co>
2026-02-19 12:24:21 -0500

    refactor: creating weater widget component

---
+++ src/components/weather.js
@@ -0,0 +1,13 @@
+export const Weather = () => {
+    return `
+<div class="weather__content">
+    <div class="weather__icon"></div>
+
+    <div class="weather__info">
+        <span class="weather__temperature">22°C</span>
+        <span class="weather__location">Madrid</span>
+        <span class="weather__description">Despejado</span>
+    </div>
+</div>
+    `;
+};
