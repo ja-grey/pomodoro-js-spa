a45dac789323c53ffa8990ce3ee56851cfd5baf0
Jorge Grey <jgrey@poligran.edu.co>
2026-02-19 11:39:06 -0500

    style: fixing favicon/logo and adding logo in header

--- src/views/homeView.js
+++ src/views/homeView.js
@@ -2,7 +2,7 @@ export const HomeView = () => {
     return `
         <header class="header">
             <h1 class="header__title">Pomodoro SPA</h1>
-            <img src="" alt="" srcset="" class="header__logo">
+            <img src="../assets/images/logo.png" alt="Pomodoro SPA Logo" class="header__logo">
         </header>            
         <main class="main-content">
             <section class="timer"></section>
