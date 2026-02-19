e6a671c3b03ccc94d70fdbc610b989b8b6be7bec
Jorge Grey <jgrey@poligran.edu.co>
2026-02-19 10:05:35 -0500

    refactor: creating render function structure in main.js

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import state from './state.js';
+import {HomeView} from './views/homeView.js';
+
+function renderTimer() {
+    
+}
+function renderTodo() {
+    
+}
+function renderWeather() {
+
+}
+function renderAmbient() {
+    
+}
+function render() {
+    document.getElementById("app").innerHTML = HomeView()
+    renderTimer()
+    renderTodo()
+    renderWeather()
+    renderAmbient()
+}
