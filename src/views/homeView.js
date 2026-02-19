513c2c312a32bad5d4b70656b5af184add1e0c56
Jorge Grey <jgrey@poligran.edu.co>
2026-02-19 09:56:26 -0500

    refactor: adding header component and basic HomeView structure

---
+++ src/views/homeView.js
@@ -0,0 +1,14 @@
+export const HomeView = () => {
+    return `
+        <header class="header">
+            <h1 class="header__title">Pomodoro SPA</h1>
+            <img src="" alt="" srcset="" class="header__logo">
+        </header>            
+        <main class="main-content">
+            <section class="timer"></section>
+            <section class="todo"></section>
+            <section class="ambient"></section>
+            <section class="weather"></section>
+        </main>
+    `;
+};
